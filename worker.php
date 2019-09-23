e88565515d062a77eeebee66c89402e56d65e123
Rápli András <rapliandras@gmail.com>
2019-09-23 10:27:55 +0200

    Update worker.php
    symfony deprecation fix

--- worker.php
+++ worker.php
@@ -34,7 +34,7 @@ if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
             'APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.'
         );
     }
-    (new Dotenv())->load(__DIR__ . '/../../../.env');
+    (new Dotenv(true))->load(__DIR__ . '/../../../.env');
 }
 
 $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
